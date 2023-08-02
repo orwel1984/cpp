62f7c3b773baed83ce978e1c0250215f19f15599
Orwel <info@render6d.com>
2023-08-03 01:43:12 +0200

    multi-threaded benchmark updated

--- CMake-Tuts/HelloBenchmark/src/multithreaded.cpp
+++ CMake-Tuts/HelloBenchmark/src/multithreaded.cpp
@@ -1,55 +1,63 @@
 #include <benchmark/benchmark.h>
 #include <algorithm>
 #include <execution>
-#include <ctime>
+#include <limits>
+#include <random>
+#include <vector>
 
 // create a vector of 500 elements
 constexpr auto size = 1000000u;
-std::vector<double> v(size);
+static std::vector<double> v;
 
-void initVector(){
-  // for(int i=0; i<10; i++){
-  //   ;
-  // }
-  // std::srand(unsigned(std::time(nullptr)));
-  // std::generate(v.begin(), v.end(), std::rand);
+static void generate_data(size_t size)
+{
+    using value_type = int;
+    // We use static in order to instantiate the random engine and the distribution once only.
+    // It may provoke some thread-safety issues.
+    static std::uniform_int_distribution<value_type> distribution(
+        std::numeric_limits<value_type>::min(),
+        std::numeric_limits<value_type>::max());
+    static std::default_random_engine generator;
+
+    // generate
+    v.reserve(size);
+    std::generate(v.begin(), v.end(), []() { return distribution(generator); });
+}
+
+void initVector(size_t size){
+  generate_data(size);
+}
+
+static void DoSetup(const benchmark::State& state) {
+  // first argument is size
+  initVector(state.range(0));
+}
+
+static void DoTeardown(const benchmark::State& state) {
 }
 
 void sequential(){
-  std::for_each(v.begin(), v.end(),[](double& x){ initVector(); ++x; });
+  std::sort(v.begin(), v.end());  
+  //std::for_each(v.begin(), v.end(),[](double& x){ ++x; });
 }
 
-void parallel(){
-  std::for_each(std::execution::par,v.begin(), v.end(),[](double& x){ initVector(); ++x; });
-}
+// void parallel(){
+//   std::for_each(std::execution::par,v.begin(), v.end(),[](double& x){ initVector(); ++x; });
+// }
 
 // Benchmark: sequential()
 static void BM_Sequential(benchmark::State& state) {
   for (auto _ : state)
     sequential();
 }
-BENCHMARK(BM_Sequential);
+BENCHMARK(BM_Sequential)->Arg(1024)->Arg(32*1024)->Arg(64*1024)->Setup(DoSetup);
 
-// Benchmark: parallel()
-static void BM_Parallel(benchmark::State& state) {
-  for (auto _ : state)
-    parallel();
-}
-BENCHMARK(BM_Parallel);
+// // Benchmark: parallel()
+// static void BM_Parallel(benchmark::State& state) {
+//   for (auto _ : state)
+//     parallel();
+// }
+// BENCHMARK(BM_Parallel);
 
-// Benchmark a function which creates empty strings
-static void BM_StringCreation(benchmark::State& state) {
-  for (auto _ : state)
-    std::string empty_string;
-}
-BENCHMARK(BM_StringCreation);
-
-// Benchmark a function which copies strings
-static void BM_StringCopy(benchmark::State& state) {
-  std::string x = "hello";
-  for (auto _ : state)
-    std::string copy(x);
-}
-BENCHMARK(BM_StringCopy);
 
 BENCHMARK_MAIN();
