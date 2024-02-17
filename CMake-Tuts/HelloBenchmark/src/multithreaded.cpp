05f1863db4a664de0817ba5ad1f38b84158d939a
spiderman <spidermannnn@proton.me>
2024-02-17 09:40:13 +0100

    Multithreaded updated with random number generator

--- CMake-Tuts/HelloBenchmark/src/multithreaded.cpp
+++ CMake-Tuts/HelloBenchmark/src/multithreaded.cpp
@@ -4,41 +4,48 @@
 #include <limits>
 #include <random>
 #include <vector>
+#include <iostream>
 
-// create a vector of 500 elements
-constexpr auto size = 1000000u;
-static std::vector<double> v;
+using value_type = int;
+static std::vector<value_type> v;
 
 static void generate_data(size_t size)
 {
-    using value_type = int;
-    // We use static in order to instantiate the random engine and the distribution once only.
-    // It may provoke some thread-safety issues.
-    static std::uniform_int_distribution<value_type> distribution(
-        std::numeric_limits<value_type>::min(),
-        std::numeric_limits<value_type>::max());
-    static std::default_random_engine generator;
+  auto randomNumberBetween = [](int low, int high)
+  {
+      auto randomFunc = [distribution_ = std::uniform_int_distribution<int>(low, high), 
+                        random_engine_ = std::mt19937{ std::random_device{}() }]() mutable
+      {
+          return distribution_(random_engine_);
+      };
+      return randomFunc;
+  };
 
     // generate
-    v.reserve(size);
-    std::generate(v.begin(), v.end(), []() { return distribution(generator); });
+    // v.reserve(size);
+    v = std::vector<value_type>(size);
+    std::generate(v.begin(), v.end(), randomNumberBetween(1,100000000));
+}
+
+void output()
+{
+  for(int i=0; i<8; i++){
+    std::cout<<"\n"<<i<<":"<<v[i];
+  }
 }
 
 void initVector(size_t size){
   generate_data(size);
 }
 
-static void DoSetup(const benchmark::State& state) {
-  // first argument is size
+static void Setup(const benchmark::State& state) {
   initVector(state.range(0));
 }
 
-static void DoTeardown(const benchmark::State& state) {
-}
+static void Teardown(const benchmark::State& state) { }
 
 void sequential(){
   std::sort(v.begin(), v.end());  
-  //std::for_each(v.begin(), v.end(),[](double& x){ ++x; });
 }
 
 // void parallel(){
@@ -47,10 +54,24 @@ void sequential(){
 
 // Benchmark: sequential()
 static void BM_Sequential(benchmark::State& state) {
+  state.PauseTiming();
+  Setup(state);
+  state.ResumeTiming();
   for (auto _ : state)
     sequential();
+  
+  //benchmark::DoNotOptimize(v);
+  int bytes = state.range(0)*sizeof(value_type);
+  state.SetBytesProcessed( long(state.iterations())* long(bytes));
+  state.SetLabel(std::to_string(bytes/1024) + "kb");
+
 }
-BENCHMARK(BM_Sequential)->Arg(1024)->Arg(32*1024)->Arg(64*1024)->Setup(DoSetup);
+
+// BENCHMARK(BM_Sequential)->Arg(1024)->Arg(32*1024)->Arg(640*1024)->Setup(Setup)->Teardown(Teardown);
+BENCHMARK(BM_Sequential)->RangeMultiplier(2)->Range(8, 8<<10)
+                        ;//->Setup(Setup)->Teardown(Teardown);
+
+;
 
 // // Benchmark: parallel()
 // static void BM_Parallel(benchmark::State& state) {
